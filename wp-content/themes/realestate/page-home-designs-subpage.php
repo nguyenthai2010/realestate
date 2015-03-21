d8a71abca7973cb4c84403f0bd30c31c1a29fd3e
thanh binh <binhdarkcu@gmail.com>
2015-03-21 10:04:31 -0700

    update home design

--- wp-content/themes/realestate/page-home-designs-subpage.php
+++ wp-content/themes/realestate/page-home-designs-subpage.php
@@ -7,74 +7,125 @@
     <div class="container pad40">
 	    <?php get_sidebar();?>
 	    
-	    <div class="span8">
-	    	<div class="span4">
-	    		<h2> Overture</h2>
-	    		<img src="images/c-1.jpg"/>
-	    		<div class="desc">
-					<p>To contact Masterton Homes Sales Department, simply click on the email address below, and send enquiry through to us. Please include your name and contact details if you would like information posted to you.</p>	    			
-	    		</div>
-	    		<a href="mailto:sales@masterton.com.au" class="link">sales@masterton.com.au</a>
-	    	</div>
-	    	<div class="span4">
-	    		<h3> HOME AND LAND ENQUIRY</h3>
-	    		<img src="images/c-2.jpg"/>
-	    		<div class="desc">
-					<p>To contact Masterton Homes Sales Department, simply click on the email address below, and send enquiry through to us. Please include your name and contact details if you would like information posted to you.</p>	    			
-	    		</div>
-	    		<a href="mailto:homeland@masterton.com.au" class="link">homeland@masterton.com.au</a>
-	    	</div>
-	    	<div class="span4">
-	    		<h3>CONSTRUCTION ENQUIRY</h3>
-	    		<img src="images/c-3.jpg"/>
-	    		<div class="desc">
-					<p>
-						If you are an existing customer and have a construction query, or you would like to receive an update on the progress of your home please click on the email address below.
-					</p>	    			
-					<p>
-						<b>If you would like to receive more information, please include your contact details, phone, fax and postal address.</b>
-					</p>
-	    		</div>
-	    		<a href="mailto:construction@masterton.com.au" class="link">construction@masterton.com.au</a>
-	    	</div>
-	    	<div class="padtop23"></div>
-	    	<div class="span4">
-	    		<h3>90 DAY MAINTENANCE ENQUIRY</h3>
-	    		<div class="desc">
-					<p>
-						At Masterton Homes we take pride in being able to supply you with the most up to date information. Not only during the construction period but also for items included in your 90 day maintenance listing.
-					</p>	
-					<p>
-						If you would like to make an enquiry regarding any outstanding items for 90 day maintenance or to receive an update, please click on the address below. 
-					</p>    			
-					<p>
-						<b>
-							If you would like a more detailed reply, please include your full name, building address, contact telephone and address details.
-						</b>
-					</p>
-	    		</div>
-	    		<a href="mailto:maintenance@masterton.com.au" class="link">maintenance@masterton.com.au</a>
-	    	</div>
-	    	<div class="span4">
-	    		<h3>CAREERS AT MASTERTON HOMES</h3>
-	    		<img src="images/c-4.jpg"/>
-	    		<div class="desc">
-					<p>
-						Masterton is committed to employing, developing and retaining the best employees. We are an equal opportunity employer and believe in investing in the development of our people. If you would like to email us and leave your details, we will keep them on file for when a job you are interested in arises. Please submit your resume via email.
-					</p>	    			
-	    		</div>
-	    		<a href="mailto:human_resources@masterton.com.au" class="link">human_resources@masterton.com.au</a>
-	    	</div>
-	    	<div class="span4">
-	    		<h3>COMMENTS ABOUT THE WEBSITE?</h3>
-	    		<img src="images/c-3.jpg"/>
-	    		<div class="desc">
-					<p>
-						If you have any comments about the Masterton website, please feel free to email and let us know.
-					</p>
-	    		</div>
-	    		<a href="mailto:webmaster@masterton.com.au" class="link">webmaster@masterton.com.au</a>
-	    	</div>
+	    <div class="span8 spanRightcontent">
+	    	<h2> Overture</h2>
+	    	<div id="gallery" class="sliderStyle1 sliderStyleHomeDesign">
+                  <div class="container">
+                      <div class="row">
+                          <div class="row-fluid text-center extrabold">
+                              <div class="submenu">
+                                  <a class="btn-homepage-menu " href="#">OVERVIEW</a>
+                                  <a class="btn-homepage-menu" href="#">INTERNALS</a>
+                                  <a class="btn-homepage-menu active" href="#">FACADES</a>
+                                  
+                              </div>
+                          </div>
+                      </div>
+			    	<div class="homedesignGallery">
+			    		<div class="bigImage">
+			    			<img src="images/bighomedesign.jpg" />
+			    			<div class="description">
+			    				<div class="shadow"></div>
+			    				<p>TIMELESS LHS</p>
+			    			</div>
+			    		</div>
+			    		<ul id="gallery-carousel" class="jcarousel-skin-tango" >
+		                  <li>
+		                      <div class="item">
+		                          <div class="pic"> <img src="images/pages/home/thum1.png" alt="" height="234" width="139" /> <a href="http://cdn.pimg.co/p/800x600/183381/fff/img.png" rel="prettyPhoto[gallery]" class="zoom img-circle"></a> </div>
+		                          <div class="item-caption">
+		                              <p>ALLURE LHS</p>
+		                          </div>
+		                      </div>
+		                  </li>
+		                  <li>
+		                      <div class="item">
+		                          <div class="pic"> <img src="images/pages/home/thum1.png" alt="" height="234" width="139" /> <a href="http://cdn.pimg.co/p/800x600/183381/fff/img.png" rel="prettyPhoto[gallery]" class="zoom img-circle"></a> </div>
+		                          <div class="item-caption">
+		                              <p>2 Affinity</p>
+		                              <ul>
+		                                  <li class="icon1">4</li>
+		                                  <li class="icon4">2</li>
+		                                  <li class="icon2">2</li>
+		                                  <li class="icon3"></li>
+		                              </ul>
+		                          </div>
+		                      </div>
+		                  </li>
+		                  <li>
+		                      <div class="item">
+		                          <div class="pic"> <img src="images/pages/home/thum1.png" alt="" height="234" width="139" /> <a href="http://cdn.pimg.co/p/800x600/183381/fff/img.png" rel="prettyPhoto[gallery]" class="zoom img-circle"></a> </div>
+		                          <div class="item-caption">
+		                              <p>3 Affinity</p>
+		                              <ul>
+		                                  <li class="icon1">4</li>
+		                                  <li class="icon4">2</li>
+		                                  <li class="icon2">2</li>
+		                                  <li class="icon3"></li>
+		                              </ul>
+		                          </div>
+		                      </div>
+		                  </li>
+		                  <li>
+		                      <div class="item">
+		                          <div class="pic"> <img src="images/pages/home/thum1.png" alt="" height="234" width="139" /> <a href="http://cdn.pimg.co/p/800x600/183381/fff/img.png" rel="prettyPhoto[gallery]" class="zoom img-circle"></a> </div>
+		                          <div class="item-caption">
+		                              <p>4 Affinity</p>
+		                              <ul>
+		                                  <li class="icon1">4</li>
+		                                  <li class="icon4">2</li>
+		                                  <li class="icon2">2</li>
+		                                  <li class="icon3"></li>
+		                              </ul>
+		                          </div>
+		                      </div>
+		                  </li>
+		                  <li>
+		                      <div class="item">
+		                          <div class="pic"> <img src="images/pages/home/thum1.png" alt="" height="234" width="139" /> <a href="http://cdn.pimg.co/p/800x600/183381/fff/img.png" rel="prettyPhoto[gallery]" class="zoom img-circle"></a> </div>
+		                          <div class="item-caption">
+		                              <p>5 Affinity</p>
+		                              <ul>
+		                                  <li class="icon1">4</li>
+		                                  <li class="icon4">2</li>
+		                                  <li class="icon2">2</li>
+		                                  <li class="icon3"></li>
+		                              </ul>
+		                          </div>
+		                      </div>
+		                  </li>
+		                  <li>
+		                      <div class="item">
+		                          <div class="pic"> <img src="images/pages/home/thum1.png" alt="" height="234" width="139" /> <a href="http://cdn.pimg.co/p/800x600/183381/fff/img.png" rel="prettyPhoto[gallery]" class="zoom img-circle"></a> </div>
+		                          <div class="item-caption">
+		                              <p> 6 Affinity</p>
+		                              <ul>
+		                                  <li class="icon1">4</li>
+		                                  <li class="icon4">2</li>
+		                                  <li class="icon2">2</li>
+		                                  <li class="icon3"></li>
+		                              </ul>
+		                          </div>
+		                      </div>
+		                  </li>
+		                  <li>
+		                      <div class="item">
+		                          <div class="pic"> <img src="images/pages/home/thum1.png" alt="" height="234" width="139" /> <a href="http://cdn.pimg.co/p/800x600/183381/fff/img.png" rel="prettyPhoto[gallery]" class="zoom img-circle"></a> </div>
+		                          <div class="item-caption">
+		                              <p>Affinity</p>
+		                              <ul>
+		                                  <li class="icon1">4</li>
+		                                  <li class="icon4">2</li>
+		                                  <li class="icon2">2</li>
+		                                  <li class="icon3"></li>
+		                              </ul>
+		                          </div>
+		                      </div>
+		                  </li>
+		              </ul>
+			    	</div>
+			   	</div>
+			 </div>
 	    </div>
     </div>
   </div>
