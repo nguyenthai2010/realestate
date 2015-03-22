68b551c7785fff232c5730024e1224f38e4c0b56
thanh binh <binhdarkcu@gmail.com>
2015-03-22 09:17:49 -0700

    update header menu

--- wp-content/themes/realestate/tpl-menu.php
+++ wp-content/themes/realestate/tpl-menu.php
@@ -2,32 +2,21 @@
   	<div class="container-fluid">
 	  	<div class="nav nav-collapse">
 	        <div class="menu"> 
-	          <ul id="nav" class="sf-menu menusm l_tinynav1">
-				  <li class="ms_first ms_home ms_top"><a href="index.html" class="menu_navigation ms_first ms_top ms_havesubmenu"><span><img src="images/i-home.png"/></span></a></li>
-				  <li class="ms_top ms_havesubmenu"><a href="#" class="menu_navigation ms_top"><span>Home Designs</span></a>
-				  	<ul style="display: none;">
-	                  <li class="ms_first"><a href="index-1.html" class="ms_first"><span>View All</span></a></li>
-	                  <li class=""><a href="index-2.html" class=""><span>Single Storey</span></a></li>
-	                  <li class=""><a href="index-3.html" class=""><span>Double Storey</span></a></li>
-	                  <li><a href="index-4.html"><span>Split Level & Sloping</span></a></li>
-	                  <li><a href="index-5.html"><span>Blocks</span></a></li>
-	                  <li><a href="index-6.html"><span>House & Grany Flat</span></a></li>
-	                  <li><a href="index-7.html"><span>Collection</span></a></li>
-	                  <li><a href="index-8.html"><span>Slimline Homes</span></a></li>
-	                  <li><a href="index-8.html"><span>DUBLEX</span></a></li>
-	                  <li><a href="index-8.html"><span>Country style homes</span></a></li>
-	                  <li><a href="index-8.html"><span>Inclusions</span></a></li>
-	                  <li><a href="index-8.html"><span>Knock down rebuild</span></a></li>
-	                  <li class="ms_last"><a href="index-20.html" class="ms_last"><span>Duplex living</span></a></li>
-				    </ul>
-				  </li>
-				  <li class="ms_top ms_havesubmenu"><a href="#" class="menu_navigation ms_top"><span>Home & Land</span></a></li>
-				  <li class="ms_top ms_havesubmenu"><a href="#" class="menu_navigation ms_top"><span>Promotions</span></a></li>
-				  <li class="ms_top ms_havesubmenu"><a href="#" class="menu_navigation ms_top"><span>Book a Meeting</span></a></li>
-				  <li class="ms_top ms_havesubmenu"><a href="#" class="menu_navigation ms_top"><span>Testimonials</span></a></li>
-				  <li class="ms_top ms_havesubmenu"><a href="#" class="menu_navigation ms_top"><span>Contacts Us</span></a></li>          
-				  <li class="ms_last ms_top"><a href="contact.html" class="menu_navigation ms_last ms_top"><span>Finance</span></a></li>
-				</ul>
+	        	<?php
+					$nav = array(
+						'theme_location'  => 'menu_top',
+						'menu'            => '',
+						'container'       => '',
+						'container_class' => '',
+						'container_id'    => '',
+						'menu_class'      => 'sf-menu menusm l_tinynav1',
+						'menu_id'         => 'nav',
+						'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					);
+					
+					wp_nav_menu( $nav );
+				?>
+	          
 	        </div>
 	     </div>
       </div>
