18a03259d52c407ed1a0cdcd25214c23c5e4fbe4
Thai Nguyen <thai@bliss-interactive.net>
2015-03-22 22:18:40 +0700

    fixed testimonials page

--- wp-content/themes/realestate/page-testimonials.php
+++ wp-content/themes/realestate/page-testimonials.php
@@ -3,46 +3,54 @@
 ?>
 
 <div id="content" class="testimonialsPage bgGray">
-<div class="container gallery3" id="gallery" style="background:none;">
-<div class="row">
-    <div class="separator"></div>
-    <h1 class="text-center extrabold">TESTIMONIALS</h1>
-    <p class="text-center extrabold"></p>
-    <div class="separator"></div>
-    <!-- /now_page -->
-</div>
-<div class="content_page">
+    <div class="container gallery3" id="gallery" style="background:none;">
+        <div class="row">
+            <div class="separator"></div>
+            <h1 class="text-center extrabold">TESTIMONIALS</h1>
+            <p class="text-center extrabold"></p>
+            <div class="separator"></div>
+            <!-- /now_page -->
+        </div>
+        <div class="content_page">
 
-<div class="row">
-    <div class="span3">
-        <div class="pic"> <a class=""><img src="images/pages/testimonials/p-1.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
-        <h3><a href="#">Tunc lacrimas illa</a></h3>
-        <p>Lorem ipsum dolor sit amet, lavabat quo est cum obiectum aliquip ipsa mihi servitute coniunx. </p>
-        <a class="btn-color-watch" href="#"><img src="images/pages/testimonials/play_s.png" width="25">WATCH NOW</a>
+    <div class="row">
+        <div class="span3">
+            <div class="pic"> <a class=""><img src="images/pages/testimonials/p-1.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
+            <h3><a href="#">Brian & Elayne</a></h3>
+            <p>Brian & Elayne talk about their experience of building a new home with Masterton Homes.</p>
+            <a class="btn-color-watch" href="#"><img src="images/pages/testimonials/play_s.png" width="25">WATCH NOW</a>
+        </div>
+        <div class="span3">
+            <div class="pic"> <a class=""><img src="images/pages/testimonials/p-2.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
+            <h3><a href="#">GREG & Sally </a></h3>
+            <p>GREG & Sally  talk about their experience of building a new home with Masterton Homes.</p>
+            <a class="btn-color-watch" href="#"><img src="images/pages/testimonials/play_s.png" width="25">WATCH NOW</a>
+        </div>
+        <div class="span3">
+            <div class="pic"> <a class=""><img src="images/pages/testimonials/p-3.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
+            <h3><a href="#">MATTHEW & ALISHA</a></h3>
+            <p>MATTHEW & ALISHA talk about their experience of building a new home with Masterton Homes.</p>
+            <a class="btn-color-watch" href="#"><img src="images/pages/testimonials/play_s.png" width="25">WATCH NOW</a>
+        </div>
+        <div class="span3">
+            <div class="pic"> <a class=""><img src="images/pages/testimonials/p-4.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
+            <h3><a href="#">PETER & DAWN</a></h3>
+            <p>PETER & DAWN talk about their experience of building a new home with Masterton Homes.</p>
+            <a class="btn-color-watch" href="#"><img src="images/pages/testimonials/play_s.png" width="25">WATCH NOW</a>
+        </div>
     </div>
-    <div class="span3">
-        <div class="pic"> <a class=""><img src="images/pages/testimonials/p-2.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
-        <h3><a href="#">Cras mattis consectetur</a></h3>
-        <p>Meam ad suis caelo in lucem exitum vivit in modo cavendum es. </p>
-    </div>
-    <div class="span3">
-        <div class="pic"> <a class=""><img src="images/pages/testimonials/p-3.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
-        <h3><a href="#">Cras mattis consectetur</a></h3>
-        <p>Meam ad suis caelo in lucem exitum vivit in modo cavendum es. </p>
-    </div>
-    <div class="span3">
-        <div class="pic"> <a class=""><img src="images/pages/testimonials/p-4.png" style="visibility: visible; opacity: 1;"></a> <a href="http://cdn.pimg.co/p/800x600/456987/fff/img.png" class="zoom img-circle" rel="prettyPhoto[id]"></a> </div>
-        <h3><a href="#">Lorem ipsum dolor sit </a></h3>
-        <p>Tunc lacrimas illa benignissime vi aureo in lucem exempli paupers coniunx efficerer nuptiarum </p>
-    </div>
-</div>
-<div class="clr"></div>
-<div class="separator_small"></div>
+    <div class="clr"></div>
+    <div class="separator_small"></div>
 
 
-</div>
-</div>
-<div class="clr separator"></div>
+    </div>
+    </div>
+    <div class="clr separator"></div>
+    <div class="row text-center">
+        <a class="btn-showmore" href="#">SHOW MORE</a>
+    </div>
+    <div class="clr"></div>
+    <div class="separator_small"></div>
 </div>
   <!-- #content-->
   
