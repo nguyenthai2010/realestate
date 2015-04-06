9668358d3d0d31f08d0f6396c616a2785316c42e
thanh binh <binhdarkcu@gmail.com>
2015-04-06 21:15:48 +0700

    Update land page

---
+++ wp-content/themes/realestate/page-home-land.php
@@ -0,0 +1,90 @@
+<?php
+	get_header();
+?>
+  
+  <div id="content" class="homedesignPage bgGray">
+    <img src="images/homeland/bigimg.jpg" width="100%"/>
+	<div class="homeLand">
+	    <div class="container pad40">
+	    	<h2 class="bigtitle">HOME AND LAND PACKAGES</h2>
+	    	<div class="headerbox">
+	    		<div class="paging span4 margin-left-0">
+	    			<p class="total-title">Total Properties: <span>57</span></p>
+	    			<ul class='page-numbers'>
+						<li><a class='page-numbers' href='http://localhost/'>1</a></li>
+						<li><a class='page-numbers' href='http://localhost/page/5/'>2</a></li>
+						<li><a class='page-numbers' href='http://localhost/page/6/'>3</a></li>
+						<li><a class='page-numbers' href='http://localhost/page/7/'>4</a></li>
+						<li><span class='page-numbers current'>5</span></li>
+						<li><a class='page-numbers' href='http://localhost/page/9/'>6</a></li>
+						<li><a class='page-numbers' href='http://localhost/page/10/'>7</a></li>
+						<li><a class='page-numbers' href='http://localhost/page/11/'>8</a></li>
+						<li><a class='page-numbers' href='http://localhost/page/14/'>9</a></li>
+					</ul>
+	    		</div>
+	    		<div class="viewhomeland span4">
+	    			<div class="middle">
+	    				<a href="#" class="viewlist">
+		    				<span>VIEW<br/>LIST</span>
+		    			</a>
+		    			<a href="#" class="viewmap">
+		    				<span>VIEW<br/>MAP</span>
+		    			</a>
+	    			</div>
+	    		</div>
+	    		<div class="orderbybox span4 pull-right">
+	    			<div class="orderbyleft">
+	    				<label>
+		    				ORDER BY
+		    			</label>
+		    			<select class="select" name="orderby">
+		    				<option value="asc">Latest Listing</option>
+							<option value="desc">Orderest Listing</option>
+		    			</select>
+	    			</div>
+	    		</div>
+	    	</div>
+	    	<div class="landList">
+	    		<div class="item">
+	    			<div class="pad">
+	    				<img src="images/homeland/i-1.jpg"/>
+	    				<div class="desc">
+	    					<h3>"Prelude Elite - 600SQM Block Masterton Home and Land Package" </h3>
+	    					<span class="pos">"Prelude Elite - 600SQM Block Masterton Home and Land Package" </span>
+	    					<div class="text">
+	    						** 
+	    						SECURE YOUR DREAM HOME TODAY WITH A 100% FIXED HOME & LAND PACKAGE PRICE ** DESIGN FEATURES INCLUDE: Spacious, open plan living Home Theatre Gourmet Artline kitchen Luxury Createc high glos...
+	    						<a href="#">View the full details </a>
+	    					</div>
+	    				</div>
+	    				<div class="price">
+	    					<div class="num">
+	    						$749,500
+	    					</div>
+	    					<ul>
+	    						<li class="bed">
+	    							<label>Bedrooms</label>
+	    							<span>4</span>
+	    						</li>
+	    						<li class="bath">
+	    							<label>Bathrooms</label>
+	    							<span>2</span>
+	    						</li>
+	    						<li class="car">
+	    							<label>Car Spaces</label>
+	    							<span>2</span>
+	    						</li>
+	    					</ul>
+	    				</div>
+	    			</div>
+	    		</div>
+	    	</div>
+	    </div>
+    </div>
+  </div>
+  <!-- #content--> 
+  
+</div>
+<!-- #wrapper -->
+
+<?php get_footer(); ?>
