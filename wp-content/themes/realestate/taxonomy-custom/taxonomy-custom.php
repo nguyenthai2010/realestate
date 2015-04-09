88fc56435a0b38a3278d876bba0c376887263f1d
Binh <binhdarkcu@gmail.com>
2015-04-09 01:33:56 -0700

    taxonomy homeland

--- wp-content/themes/realestate/taxonomy-custom/taxonomy-custom.php
+++ wp-content/themes/realestate/taxonomy-custom/taxonomy-custom.php
@@ -1,15 +1,33 @@
 <?php
+add_action( 'init', 'add_custom_homeland_taxonomies');
 
-function homeland_init() {
-	// create a new taxonomy
-	register_taxonomy(
-		'homeland',
-		'homeland',
-		array(
-			'label' => __( 'Home Land Category' ),
-			'rewrite' => array( 'slug' => 'homeland-category' ),
-			
-		)
+// create two taxonomies, genres and writers for the post type "book"
+function add_custom_homeland_taxonomies() {
+	// Add new taxonomy, make it hierarchical (like categories)
+	$labels = array(
+		'name'              => _x( 'Home land category', 'Home land category' ),
+		'singular_name'     => _x( 'Home land category', 'Home land category' ),
+		'search_items'      => __( 'Search Home land category' ),
+		'all_items'         => __( 'All Home land category' ),
+		'parent_item'       => __( 'Parent Home land category' ),
+		'parent_item_colon' => __( 'Parent Home land category' ),
+		'edit_item'         => __( 'Edit Home land category' ),
+		'update_item'       => __( 'Update Home land category' ),
+		'add_new_item'      => __( 'Add Home land category' ),
+		'new_item_name'     => __( 'Add Home land category Name' ),
+		'menu_name'         => __( 'Home land category' ),
 	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => false,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'homelandtax' ),
+	);
+
+	register_taxonomy('homelandtax', array( 'post','homeland' ), $args );
 }
-add_action( 'init', 'homeland_init' );
+
+add_action( 'init', 'add_custom_homeland_taxonomies');
