2124bce341988c80f7b9b56b161ccf90fb063ee7
thanh binh <binhdarkcu@gmail.com>
2015-04-10 00:12:34 +0700

    home land page

--- wp-content/themes/realestate/footer.php
+++ wp-content/themes/realestate/footer.php
@@ -133,7 +133,7 @@
 <script type="text/javascript" src="js/search_homedesign.js"></script>
 <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.8/jquery.validate.min.js"></script>
 <script type="text/javascript" src="js/validate.jim.js"></script>
-<script type="text/javascript" src="js/paging.ajax.js"></script>
+<!--script type="text/javascript" src="js/paging.ajax.js"></script-->
 <script type="text/javascript">
 	$(function () {
 		$('#nav').tinyNav({
