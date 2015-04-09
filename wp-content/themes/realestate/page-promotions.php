bf68f369a521bc4bb012f05361199ac6d4929be4
Thai Nguyen <thai@bliss-interactive.net>
2015-04-09 23:53:17 +0700

    a

--- wp-content/themes/realestate/page-promotions.php
+++ wp-content/themes/realestate/page-promotions.php
@@ -2,19 +2,21 @@
 	get_header();
 ?>
 
-
-<div id="content" class="testimonialsPage bgGray">
+<div id="content" class="row-promotion testimonialsPage bgGray">
     <div class="container gallery3" id="gallery" style="background:none;">
         <div class="row">
             <div class="separator"></div>
             <h1 class="text-center extrabold"><?php echo get_the_title(get_the_ID())?></h1>
-            <p class="text-center extrabold">“our happy customer and their experience with Masterton Homes”</p>
+            <p class="text-center extrabold"></p>
             <div class="separator"></div>
             <!-- /now_page -->
         </div>
         <div class="content_page">
 
-            <?php get_template_part('tpl','testimonial')?>
+            <?php
+            $posts_per_page = 200;
+            get_template_part('tpl','promotion');
+            ?>
     <div class="clr"></div>
     <div class="separator_small"></div>
 
