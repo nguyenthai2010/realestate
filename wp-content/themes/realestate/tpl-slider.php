e4e988d49ef5de43ab8bdde4e618a0a0cc034d1c
Thai Nguyen <thai@bliss-interactive.net>
2015-04-26 21:23:25 +0700

    .

--- wp-content/themes/realestate/tpl-slider.php
+++ wp-content/themes/realestate/tpl-slider.php
@@ -50,11 +50,11 @@
                 <li class="firstchild">
                 	I’M LOOKING FOR:
                 	<ul class="submenu">
-	                	<li class="icon1"><i><img src="images/pages/home/ico-slider_01.png"/></i><span>Yes</span></li>
-		                <li class="icon2"><i><img src="images/pages/home/ico-slider_02.png"/></i><span>Mansion</span></li>
-		                <li class="icon3"><i><img src="images/pages/home/ico-slider_03.png"/></i><span>Knock down<br>rebuild</span> </li>
-		                <li class="icon4"><i><img src="images/pages/home/ico-slider_04.png"/></i><span>Duplex</span></li>
-		                <li class="icon5"><i><img src="images/pages/home/ico-slider_05.png"/></i><span>More+ </span></li>
+	                	<li class="icon1"><a href="/promotions/"><i><img src="images/pages/home/ico-slider_01.png"/></i><span>Yes</span></a></li>
+		                <li class="icon2"><a href="/promotion/masterton-mansion-package/"><i><img src="images/pages/home/ico-slider_02.png"/></i><span>Mansion</span></a></li>
+		                <li class="icon3"><a href="#"><i><img src="images/pages/home/ico-slider_03.png"/></i><span>Knock down<br>rebuild</span></a></li>
+		                <li class="icon4"><a href="#"><i><img src="images/pages/home/ico-slider_04.png"/></i><span>Duplex</span></a></li>
+		                <li class="icon5"><a href="/designs-subpage/"><i><img src="images/pages/home/ico-slider_05.png"/></i><span>More+ </span></a></li>
 	                </ul>
                 </li>                
             </ul>
